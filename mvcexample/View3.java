0df7d18152101a0898fa48d3aa87fbba57b59677
whovi <whovi@BOOK-97H07VHARP.home>
2026-02-19 03:08:44 +0000

    Updated view 3 to implement Observer interface. (part2)

--- mvcexample/View3.java
+++ mvcexample/View3.java
@@ -2,14 +2,15 @@ package mvcexample;
 
 // This is the yellow view.
 // It displays the A component of the model (data base).
-// As supplied, this view does not subscribe to the model as an Observer,
-// and relies on its controller to call update to cause the view to be refreshed.
+// extra comment becuase it didn't work last time
+// another extra comment
 
 import java.util.*;  // For Observer
 import java.awt.*;
 import javax.swing.*;
 
-public class View3 extends JPanel {
+public class View3 extends JPanel
+                   implements Observer {
   
     private Model model;
     private Controller2 contr;   // Parent Frame
@@ -29,10 +30,13 @@ public class View3 extends JPanel {
         display = new JTextField("No data", 15);
         add(display);
         
+        // Subscribe to the model
+        model.addObserver(this);
+        
     } // constructor
     
-    // Called by controller to refresh the view:
-    public void update() {
+    //Notified by the model when it is altered:
+    public void update(Observable o, Object arg) {
         
         // Fetch (potentially) updated information and display it
         int a = model.getDataA();
